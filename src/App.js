dc2245c55c8e6e4bcbd5b7797cd5b6e0b5d5a7e7
Clay McKee <cmckee120993@gmail.com>
2022-11-18 12:01:29 -0500

    setting up files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Header';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <Header/>
+    <Navbar/>
+    <Footer/>
+  );
+}
+
+export default App;
