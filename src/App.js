b003a8fe361035ab74e3836e1f75e15d31ec45ca
Clay McKee <cmckee120993@gmail.com>
2022-11-19 09:52:45 -0500

    footer styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import './App.css'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Footer/>
     </div>
+  
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
-// import '../styles/app.css';
+import github from '../assets/images/github.png';
+import linkedin from '../assets/images/linkedin.webp';
+import logo from '../assets/images/clays-creative-solutions.png'
+import '../styles/Footer.css';
 
 function Footer() {
 return (
 <footer className="footer">
-    <h3>Clayton McKee was here!</h3>
+    <div className="links">
+            <a href="https://github.com/cmckee120993"><img alt="Link to Github" src={github} /></a>
+            <a href="https://www.linkedin.com/in/clayton-mckee-ba992a123/"><img alt="Link to LinkedIn" src={linkedin} /></a>
+        </div>
+        <div className="logo-div">
+            <img className="logo" alt="Clay's Creative Solutions Logo" src={logo} />
+        </div>
+        <div className="header-div">
+            <h3>© Clay's Creative Solutions</h3>
+        </div>
 </footer>
 );
 };
