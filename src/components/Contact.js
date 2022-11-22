f0323fee0bea2f3eb76880b2ae3063af19ea4588
Clay McKee <cmckee120993@gmail.com>
2022-11-21 22:02:22 -0500

    began form set up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Contact.css';
+import {useForm, ValidationError} from '@formspree/react';
+import {validateEmail} from '../util/helpers';
+require('dotenv').config();
 
 
 function Contact() {
+    const [state, handleSubmit] = useForm(process.env.REACT_PORTFOLIO_FORM_ID);
+
+    if(state.succeeded) {
+        return (
+            <div>
+                <p>Thanks for your message! Clay will get back to you as soon as possible.</p>
+                <button onClick={() => window.location("/#about")}>Back to About Page</button>
+            </div>
+        );
+    }
     return (
-       <h1>Hello!</h1>
+       <form onSubmit={handleSubmit}>
+
+       </form>
     )
 };
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,68 @@
-import React from 'react';
-import '../styles/Resume.css';
-
+import React from "react";
+import "../styles/Resume.css";
 
 function Resume() {
-    return (
-       <h1>Hello!</h1>
-    )
-};
+  return (
+    <content>
+      <div>
+        <h2>Programming Skills</h2>
+        <ul className="skills">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>APIs</li>
+          <li>Node.js</li>
+          <li>OOP</li>
+          <li>Express.js</li>
+          <li>MYSQL</li>
+          <li>ORM</li>
+          <li>MVC</li>
+          <li>MongoDB</li>
+          <li>PWA</li>
+          <li>React</li>
+          <li>MERN</li>
+          <li>State</li>
+        </ul>
 
-export default Resume; 
+        <h2>Education</h2>
+        <ul className="education">
+            <li>University of California, Los Angeles</li>
+                <ul>
+                    <li>June to December 2022</li>
+                    <li>Certificate Full Stack Web Development</li>
+                </ul>
+            <li>University of California, Los Angeles</li>
+            <ul>
+                <li>Fall 2018 to Present</li>
+                <li>Doctoral Candidate in the Department of Comparative Literature</li>
+                <li>Certificates:</li>
+                    <ul>
+                        <li>Urban Humanitites</li>
+                        <li>Writing Pedagogy: Language Learner Emphasis</li>
+                    </ul>
+            </ul>
+            <li>Penn State University</li>
+                <ul>
+                    <li>Summer 2012 to Spring 2017</li>
+                    <li>MA: Comparative Literature</li>
+                    <li>BAs:</li>
+                        <ul>
+                            <li>Comparative Literature</li>
+                            <li>French</li>
+                            <li>Spanish</li>
+                        </ul>
+                    <li>Minor: Arabic</li>
+                </ul>
+            <li>Additional Education:</li>
+                <ul>
+                    <li>Middlebury College Summer Language, Arabic (2020)</li>
+                    <li>Concordia University, Translation and the City (2019)</li>
+                    <li>University of Houston-Victoria, Certificate in Literary Translation (2017)</li>
+                </ul>
+        </ul>
+      </div>
+    </content>
+  );
+}
+
+export default Resume;
