fcff9f29a4d2cced0a9bb3b266eccc33694d741c
Clay McKee <cmckee120993@gmail.com>
2022-11-27 22:39:19 +0100

    nav and heading styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ import Portfolio from './Portfolio';
 import Contact from './Contact';
 import Resume from './Resume';
 import '../styles/Header.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFileCode} from '@fortawesome/free-solid-svg-icons';
+import {faKeyboard} from '@fortawesome/free-solid-svg-icons'
+import {faGlobe} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     const [currentPage, handlePageChange] = useState('About');
@@ -30,6 +34,12 @@ function Header() {
         <header className="header">
             <h1>Clayton McKee</h1>
         </header>
+        <div className="icons">
+            <FontAwesomeIcon className="icon" icon={faFileCode}/>
+            <FontAwesomeIcon className="icon" icon={faKeyboard}/>
+            <FontAwesomeIcon className="icon" icon={faGlobe}/>
+        </div>
+        
         <Navigation currentPage={currentPage}
         handlePageChange={handlePageChange} />
         <main>
